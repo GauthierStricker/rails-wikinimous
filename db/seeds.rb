956c2c017f7ec422a02970c6401d3c9cba1655b1
Gauthier Stricker <stricker.gauthier@gmail.com>
2017-11-22 00:06:06 +0100

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 0..10
+  Article.create(title: Faker::SiliconValley.motto, content: Faker::SiliconValley.quote)
+end
